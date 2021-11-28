6986663140d6add3ba7af246915df0907a49f83b
Skylar Harwell <swharwell@gmail.com>
2021-11-28 16:04:23 -0500

    Updated routing for pages and Layout for app view

---
+++ client/src/components/Layout.js
@@ -0,0 +1,137 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import Drawer from '@material-ui/core/Drawer'
+import Typography from '@material-ui/core/Typography'
+import { useNavigate, useLocation } from 'react-router-dom'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import { format } from 'date-fns'
+import Avatar from '@material-ui/core/Avatar'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: '#f9f9f9',
+      width: '100%',
+      padding: theme.spacing(3),
+    },
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    active: {
+      background: '#f4f4f4'
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+    appBar: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+    date: {
+      flexGrow: 1
+    },
+    toolbar: theme.mixins.toolbar,
+    avatar: {
+      marginLeft: theme.spacing(2)
+    }
+  }
+})
+
+export default function Layout({ children }) {
+  const classes = useStyles()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const menuItems = [
+    {
+      text: "Search",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Login",
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/Login",
+    },
+    {
+      text: "Signup",
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/Signup",
+    },
+    {
+      text: "My Collection",
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/mycollection",
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      {/* app bar */}
+      <AppBar 
+        position="fixed" 
+        className={classes.appBar}
+        elevation={0}
+        color="primary"
+      >
+        <Toolbar>
+          <Typography className={classes.date}>
+            Today is the {format(new Date(), 'do MMMM Y')}
+          </Typography>
+          <Typography>User Name</Typography>
+          <Avatar className={classes.avatar}/>
+        </Toolbar>
+      </AppBar>
+
+      {/* side drawer */}
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{ paper: classes.drawerPaper }}
+        anchor="left"
+      >
+        <div>
+          <Typography variant="h5" className={classes.title}>
+            Hoarder's Helper
+          </Typography>
+        </div>
+
+        {/* links/list section */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem 
+              button 
+              key={item.text} 
+              onClick={() => navigate(item.path)}
+              className={location.pathname == item.path ? classes.active : null}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+        
+      </Drawer>
+
+      {/* main content */}
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        { children }
+      </div>
+    </div>
+  )
+}
