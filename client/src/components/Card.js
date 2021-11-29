700170f9d9952e99537b5d06c59cd3ebeae4ab04
Berlic <bsorrell3@gmail.com>
2021-11-28 22:54:26 -0500

    added working cards component, works with sample data on home page

---
+++ client/src/components/Card.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { Button } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+export default function MakeCard({card}) {
+  
+  
+  if(card.imageUrl == undefined) {
+    card.imageUrl = "https://i.imgur.com/LdOBU1I.jpg"
+  };
+  
+  return (
+    <Card sx={{ maxWidth: 200 }}>
+      <CardMedia
+        component="img"
+        height="25%"
+        width="auto"
+        image={card.imageUrl} 
+        alt={card.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {card.name}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          Type: {card.types}
+        </Typography>
+        <br/>
+        <Typography variant="caption" color="text.secondary">
+          Rarity: {card.rarity}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
