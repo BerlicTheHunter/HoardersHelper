aaca51473c13128c5fae85dbcf9b924069bd9abf
Berlic <bsorrell3@gmail.com>
2021-11-30 20:29:06 -0500

    Happy Servergit add -A!

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const MTGCard = require('./MTGCard');
-const MTGCollection = require('./MTGCollection');
-const CustomCollection = require('./CustomCollection');
-const CustomItem = require('./CustomItem');
 
-module.exports = { User, MTGCard, MTGCollection, CustomCollection, CustomItem};
+
+
+module.exports = { User, MTGCard};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,45 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
+    firstname: String
+    lastname: String
     email: String
     password: String
+    mtgCard: [MTGCard] 
   }
+
+  type MTGCard{
+    name: String
+    cmc: Int
+    colors: [String]
+    colorIdentity: [String]
+    type: String
+    types: [String]
+    subtypes: [String]
+    set: String
+    setName: String
+    number: String
+    imageUrl: String
+    rarity: String
+    mvId: String
+    quantity: Int
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    
+  }
+
 `;
 
 module.exports = typeDefs;
