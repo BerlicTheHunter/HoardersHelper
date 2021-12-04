21c652cff43844ec8354b5547c9cee76eb0cab57
unknown <uekemike@yahoo.com>
2021-12-04 13:59:09 -0500

    update

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import Typography from '@material-ui/core/Typography'
+
+// export default function Footer() {
+//   return (
+//       <AppBar position="static" color="primary">
+//         <Container maxWidth="md">
+//           <Toolbar>
+//             <Typography variant="body1" color="inherit">
+//               © 2021 Hoarders Helper
+//             </Typography>
+//           </Toolbar>
+//         </Container>
+//       </AppBar>
+//   )
+// }
+
+// export default Footer
